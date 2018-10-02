8461c658bff1e677d802da5f38893e8240968d5f
stephen grider <ste.grider@gmail.com>
2018-10-02 14:38:55 -0600

    boilerplate

--- plinko/score.js
+++ plinko/score.js
@@ -1,3 +1,8 @@
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-
+  // Ran every time a balls drops into a bucket
 }
+
+function runAnalysis() {
+  // Write code here to analyze stuff
+}
+
