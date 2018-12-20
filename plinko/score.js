0c14517718665f4a37fa72546420444b859c4a1c
Dhananjay Singh <dhananjay1204@gmail.com>
2018-12-20 12:05:37 +0530

    iteration 1

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,28 @@
+const outputs = [];
+const predictionPoint = 300;
+const k = 3;
+
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+  const bucket = _.chain(outputs)
+    .map(row => [distance(row[0]), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+
+  console.log('Your point will fall into', bucket);
 }
 
+function distance(point) {
+  return Math.abs(point - predictionPoint);
+}
