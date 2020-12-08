3f5b5379a50b835dc18bd81324ca5fa52d9d48e9
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-12-08 07:09:22 -0600

    recording onScoreupdate

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,10 @@
+const outputs = [];
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+  console.log(outputs);
 }
 
 function runAnalysis() {
   // Write code here to analyze stuff
 }
-
