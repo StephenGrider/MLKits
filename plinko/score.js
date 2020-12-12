2972c4c854baa978fb483d3df40ee960d865c110
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-12-11 18:19:33 -0600

    test accuracy by feature

--- plinko/score.js
+++ plinko/score.js
@@ -1,34 +1,38 @@
 const outputs = [];
 // determine the corellation between bucket and predictionPoint
-const predictionPoint = 300;
-const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-  const testSetSize = 10;
-  const [testSet, trainingSet] = splitDataSet(outputs, testSetSize);
+  const testSetSize = 100;
+  const k = 10;
+  const colNames = ['Drop Position', 'Bounciness', 'Ball Size'];
 
-  let numberCorrect = 0;
+  // vary k using range
+  _.range(0, 3).forEach((feature) => {
+    const data = _.map(outputs, (row) => [row[feature], _.last(row)]);
 
-  for (let i = 0; i < testSet.length; i++) {
-    const bucket = knn(trainingSet, testSet[i][0]);
-    if (bucket === testSet[i][3]) {
-      numberCorrect++;
-    }
-  }
+    const [testSet, trainingSet] = splitDataSet(minMax(data, 1), testSetSize);
+    const accuracy = _.chain(testSet)
+      .filter((testPoint) => knn(trainingSet, _.initial(testPoint), k) === _.last(testPoint))
+      .size()
+      .divide(testSetSize)
+      .value();
 
-  console.log(`Accuracy: ${(numberCorrect / testSetSize) * 100}%`);
+    console.log(`k(${k}) Accuracy for ${colNames[feature]}: ${accuracy * 100}%`);
+  });
 }
 
-function knn(data, point) {
+function knn(data, point, k) {
   // K-Nearest Neighbor Algorithm
   return (
     _.chain(data)
-      // [[dropPosition, bucketLabel],[72,4],[227,5]]
-      .map((row) => [distance(row[0], point), row[3]])
+      // [[distance(dropPosition, predictionPoint), bucketLabel],[72,4],[227,5]]
+      .map((row) => {
+        return [distance(_.initial(row), point), _.last(row)];
+      })
       // sort by drop position
       .sortBy((row) => row[0])
       // Gets the top 'k' results from sorted list
@@ -55,7 +59,19 @@ function knn(data, point) {
 }
 
 function distance(pointA, pointB) {
-  return Math.abs(pointA - pointB);
+  // pointA/B are arrays
+  // employing the pythagorean therom to solve a multidimensional point distance
+  _.chain(pointA)
+    // takes each value at the same index of each array and creates a new zipped
+    // array:
+    // [[pointA[0], pointB[0]], [pointA[1], pointB[1]] ... ]
+    .zip(pointB)
+    // subtracts b from a
+    .map(([a, b]) => (a - b) ** 2)
+    // sums the squares
+    .sum()
+    // returns the squareroot of the sum
+    .value() ** 0.5;
 }
 
 function splitDataSet(data, testCount) {
@@ -64,9 +80,32 @@ function splitDataSet(data, testCount) {
   const testSet = _.slice(shuffled, 0, testCount);
   const trainingSet = _.slice(shuffled, testCount);
 
-  console.log(`There are ${shuffled.length} total records in the sample.`);
-  console.log('testSet', testSet);
-  console.log('trainingSet', trainingSet);
-
   return [testSet, trainingSet];
+}
+
+function minMax(data, featureCount) {
+  const clonedData = _.cloneDeep(data);
+
+  // iterate over each feature (independent variables)
+  for (let i = 0; i < featureCount; i++) {
+    const column = clonedData.map((row) => row[i]);
+    const min = _.min(column);
+    const max = _.max(column);
+
+    // iterate over each row [j] in clonedData
+    // and normalize each feature [i]
+    // a row would look something like :
+    //
+    // [ position, bounciness, ballSize, bucketName ]
+    //
+    // where bucketName is a label and not normalized
+    for (let j = 0; j < clonedData.length; j++) {
+      clonedData[j][i] = (clonedData[j][i] - min) / (max - min);
+      if (max - min === 0) {
+        clonedData[j][i] = 0;
+      }
+    }
+  }
+
+  return clonedData;
 }
