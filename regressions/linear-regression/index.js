65f7760341052d02cedc68214d1b87ec9c4d505a
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-12-24 18:58:42 -0600

    new folder structure and npm scripts

---
+++ regressions/linear-regression/index.js
@@ -0,0 +1,19 @@
+require('@tensorflow/tfjs-node');
+const loadCSV = require('../load-csv');
+const LinearRegression = require('./linear-regression');
+
+let { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv', {
+  shuffle: true,
+  splitTest: 50,
+  dataColumns: ['horsepower'],
+  labelColumns: ['mpg'],
+});
+
+const regression = new LinearRegression(features, labels, {
+  learningRate: 0.0001,
+  iterations: 100,
+});
+
+regression.train();
+
+console.log('Updated M is ', regression.m, 'Updated B is ', regression.b);
