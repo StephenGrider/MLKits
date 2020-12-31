5176b61d0dc1b6ff42699e07e204223c24a38066
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-12-31 11:36:51 -0600

    added mnist images dir

---
+++ regressions/mnist-images/index.js
@@ -0,0 +1,60 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
+const plot = require('node-remote-plot');
+const LogisticRegression = require('./logistic-regression');
+const _ = require('lodash');
+const mnist = require('mnist-data');
+
+// --------- Helper Functions -----------------
+const encodeLabelValues = (labelValues) => {
+  return labelValues.map((label) => {
+    /**
+     * new Array(10).fill(0);
+     * creates an array ten elements in length and makes each element a 0.
+     * [0,0,0,0,0,0,...]
+     */
+    const row = new Array(10).fill(0);
+
+    /**
+     * set the row at the label index to 1 indicating the encoded value. In the
+     * case that the label value = 3, the resuting array would look like this:
+     * [0,0,0,1,0,0,...]
+     */
+    row[label] = 1;
+
+    return row;
+  });
+};
+
+function loadData() {
+  const mnistData = mnist.training(0, 60000);
+
+  // lodash's flat map removes one level of array nesting.
+  const features = mnistData.images.values.map((image) => _.flatMap(image));
+  const encodedLabels = encodeLabelValues(mnistData.labels.values);
+
+  return { features, labels: encodedLabels };
+}
+
+function loadTestData() {
+  const testMnistData = mnist.testing(0, 1000);
+  const testFeatures = testMnistData.images.values.map((image) => _.flatMap(image));
+  const testEncodedlabels = encodeLabelValues(testMnistData.labels.values);
+  return { testFeatures, testLabels: testEncodedlabels };
+}
+// ----- Applicatin Code -------------
+
+const { features, labels } = loadData();
+
+const regress = new LogisticRegression(features, labels, {
+  learningRate: 1,
+  iterations: 20,
+  batchSize: 100,
+});
+
+regress.train();
+debugger;
+
+const { testFeatures, testLabels } = loadTestData();
+const accuracy = regress.test(testFeatures, testLabels);
+console.log('Accuracy: ', accuracy);
